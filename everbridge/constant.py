1aa3e760f2cfdf101746f0e44a10444939165543
cs-shubham-morankar <smorankar@fortinet.com>
2022-10-10 12:20:29 +0530

    Initial commit

---
+++ everbridge/constant.py
@@ -0,0 +1,25 @@
+""" Copyright start
+  Copyright (C) 2008 - 2022 Fortinet Inc.
+  All rights reserved.
+  FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
+  Copyright end """
+
+errors = {
+    '401': 'Unauthorized, API key invalid',
+    '405': 'Method Not Allowed, Method other than POST used',
+    '413': 'Request Entity Too Large, Sample file size over max limit',
+    '415': 'Unsupported Media Type',
+    '418': 'Unsupported File Type Sample, file type is not supported',
+    '419': 'Request quota exceeded',
+    '420': 'Insufficient arguments',
+    '421': 'Invalid arguments',
+    '500': 'Internal error',
+    '502': 'Bad Gateway',
+    '513': 'File upload failed'
+}
+
+Incident_date_type = {
+    'Created Date': 'createdDate',
+    'Last Modified Date': 'lastModifiedDate',
+    'Closed Date': 'closedDate'
+}
